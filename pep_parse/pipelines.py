e3e3207fe9d4362c6ca2883b626596527895dd28
Michael R <rodichevmichael@gmail.com>
2024-02-20 08:05:13 +0300

    1st review

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -11,10 +11,6 @@ from pep_parse.settings import (
 
 class PepParsePipeline:
 
-    def __init__(self):
-        self.results_dir = BASE_DIR / RESULTS
-        self.results_dir.mkdir(exist_ok=True)
-
     def open_spider(self, spider):
         self.status_count = defaultdict(int)
 
@@ -25,7 +21,7 @@ class PepParsePipeline:
     def close_spider(self, spider):
         now_formatted = dt.datetime.now().strftime(DATETIME_FORMAT)
         with open(
-                f'{STATUS_SUMMARY_FILENAME}_{now_formatted}.{FILE_FORMAT}',
+                f'{RESULTS}/{STATUS_SUMMARY_FILENAME}_{now_formatted}.{FILE_FORMAT}',
                 'w', encoding='utf-8'
         ) as f:
             csv.writer(f, dialect=csv.unix_dialect).writerows(
